de4b1b08ccef17ab5718c29a5bff4fe115635bc3
Julien Constant <julienconstant190@gmail.com>
2022-02-28 11:10:12 +0100

    MàJ du 28/02

--- js/ShowFullscreen.js
+++ js/ShowFullscreen.js
@@ -1,21 +1,19 @@
+
+document.addEventListener('keydown', logKey);
+var modal = document.getElementById("ShowFullscreen");
+var img = document.getElementById("SF-Img");
+
 function ShowFullscreen(imgURL){
-	var modal = document.getElementById("ShowFullscreen")
-
 	modal.style.display = "block";
-
-	// close modal: 
-	var span = document.getElementById("SF-Span");
-	span.onclick = function() {
-		modal.style.display = "none";
-	}
-	// when pressing escape key:
-	$(document).keydown(function(event) { 
-		if (event.keyCode == 27) { 
-			modal.style.display = "none";
-		}
-	});
-
-	// set img in modal:
-	var img = document.getElementById("SF-Img");
 	img.src = imgURL;
+}
+
+function HideFullScreen() {
+	modal.style.display = "none";
+	img.src = "";
+}
+
+// when pressing escape key:
+function logKey(event) { 
+	if (event.keyCode == 27) HideFullScreen();
 }
